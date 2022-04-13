20de7799ed58e6ceb967dfbb1dd844cad0f28775
Henrique <henrique.reis@webjump.com.br>
2022-04-13 20:57:48 -0300

    Steps Login Completo

--- features/step_definitions/steps_login.rb
+++ features/step_definitions/steps_login.rb
@@ -3,16 +3,13 @@ Dado("que eu acesso a pagina inicial") do
   end
   
   Quando("preencho meus dados {string} e {string}") do |email, senha|
+    @email = email
     find('input[id=email]').set email
     find('input[id=pass]').set senha
     click_button 'Sign In'
     sleep 5
   end
-  
-  Então("meu login foi realizado") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então("devo ler a seguinte mensagem {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+   
+  Então("devo ler a seguinte mensagem {string}") do |mensagem|
+    expect(page).to have_content mensagem    
   end
