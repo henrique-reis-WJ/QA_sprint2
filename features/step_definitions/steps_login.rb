a7fcbcfc63db23fd16d20139447fcba20a2d2d95
Henrique <henrique.reis@webjump.com.br>
2022-04-13 21:16:13 -0300

    Iniciando login na tela inicial

--- features/step_definitions/steps_login.rb
+++ features/step_definitions/steps_login.rb
@@ -1,5 +1,6 @@
 Dado("que eu acesso a pagina inicial") do
-    visit 'https://magento.nublue.co.uk/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLm51Ymx1ZS5jby51ay8%2C/'
+    visit 'https://magento.nublue.co.uk/'
+    click_link 'Sign In'
   end
   
   Quando("preencho meus dados {string} e {string}") do |email, senha|
@@ -7,9 +8,8 @@ Dado("que eu acesso a pagina inicial") do
     find('input[id=email]').set email
     find('input[id=pass]').set senha
     click_button 'Sign In'
-    sleep 5
   end
-   
-  Então("devo ler a seguinte mensagem {string}") do |mensagem|
+
+   Então("devo ler a seguinte mensagem {string}") do |mensagem|
     expect(page).to have_content mensagem    
   end
